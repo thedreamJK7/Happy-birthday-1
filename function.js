22982daf5178ca5702305ef3072f603f0339581c
thedreamJK7 <jkubaev7@gmail.com>
2021-10-12 01:45:33 +0500

    2

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-alert("TUG'ILGAN KUNIZ MUBORAK BO'LSIN! EKRANNI BOSISHNI UNUTMANG MOHIGUL! 🎶🎉🎊");
+alert("TUG'ILGAN KUNIZ MUBORAK BO'LSIN! TORT JUDA SHIRIN BO'LGAN! MENGA HAM BIR BO'LAK YUBORING! :) KEYIN EKRANNI BOSISHNI UNUTMANG MOHIGUL! 🎶🎉🎊");
 window.addEventListener('click', function() {
     var audio = document.getElementById("HBD");
     audio.play();
